dc0dbe4c29e81dbb356ab7f00dd152d1f5476ec4
Michel Oliveira <michel.s.oliveira@gmail.com>
2018-11-19 11:10:50 -0200

    Upload do codigo local

---
+++ FormularioAspNetMvc/Models/Sexo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormularioAspNetMvc.Models
+{
+    public enum Sexo
+    {
+        Masculino, Feminino
+    }
+}
